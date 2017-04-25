35405886f0a17d80778dbd1b4613a3b3b82249c1
bvluong <bryantvinluong@gmail.com>
2017-04-25 15:14:59 -0700

    initial commit

---
+++ lib/controller_base.rb
@@ -0,0 +1,58 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require 'active_support/inflector'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise if already_built_response?
+    @res["Location"] = url
+    @res.status = 302
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.body = [content]
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = ActiveSupport::Inflector.underscore(self.class.to_s)
+    content = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    template = ERB.new(content).result(binding)
+    render_content(template, "text/html")
+
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
